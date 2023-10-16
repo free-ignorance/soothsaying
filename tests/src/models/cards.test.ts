a897b51c57a0faf3ccf0201dd27b55a2939b08ed
beaubouchard <bnb2011@gmail.com>
2023-10-16 14:10:05 -0400

    chore: more tests being added to surface

--- tests/src/models/cards.test.ts
+++ tests/src/models/cards.test.ts
@@ -2,7 +2,6 @@ import {
   newDeck, getCardByID
 } from '../../../src/models/cards';
 
-     
 describe('/models', () => {
   describe('/cards.ts', () => {
     describe('newDeck', () => {
@@ -10,9 +9,33 @@ describe('/models', () => {
       beforeEach(() => {
         let deck = newDeck();
       });
-      it('should Deck Type Object with 78 cards', () => {
+      it('should be a Deck Type Object with 78 cards', () => {
         expect(deck).toBeDefined();
         expect(deck.cards.length).toEqual(78);
+      });
+      it('should have 22 Major Arcana in New Deck', () => {
+        expect(deck).toBeDefined();
+        expect(deck.cards.filter(card => card.type === "major").length).toEqual(22);
+      });
+      it('should have 56 Minor Arcana in New Deck', () => {
+        expect(deck).toBeDefined();
+        expect(deck.cards.filter(card => card.type === "minor").length).toEqual(56);
+      });
+      it('should have 14 Wands in New Deck', () => {
+        expect(deck).toBeDefined();
+        expect(deck.cards.filter(card => card.suit === "wands").length).toEqual(14);
+      });
+      it('should have 14 Cups in New Deck', () => {
+        expect(deck).toBeDefined();
+        expect(deck.cards.filter(card => card.suit === "cups").length).toEqual(14);
+      });
+      it('should have 14 Swords in New Deck', () => {
+        expect(deck).toBeDefined();
+        expect(deck.cards.filter(card => card.suit === "swords").length).toEqual(14);
+      });
+      it('should have 14 Pentacles in New Deck', () => {
+        expect(deck).toBeDefined();
+        expect(deck.cards.filter(card => card.suit === "pentacles").length).toEqual(14);
       });
     });
     describe('getCardByID', () => {
